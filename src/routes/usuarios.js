37fdf96131a08cb0f9a7997c97e9dc4d1c4d4a79
Pericles Cabral <periclesccabral@gmail.com>
2024-06-17 20:10:03 -0300

    inserindo endpoints detalharUsuario e atualizarUsuario

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { cadastrarUsuario, logarUsuario } = require('../controllers/usuarios');
+const { cadastrarUsuario, logarUsuario, detalharUsuario, atualizarUsuario } = require('../controllers/usuarios');
 const { validarCamposCadastro, validarCamposLogin } = require('../middlewares/validacoesUsuarios');
+const validarLogin = require('../middlewares/autenticacaoLogin');
 
 const rotas = express();
 
+rotas.get('/usuario', validarLogin, detalharUsuario);
+rotas.put('/usuario', validarLogin, validarCamposCadastro, atualizarUsuario);
 rotas.post('/usuario', validarCamposCadastro, cadastrarUsuario);
 rotas.post('/login', validarCamposLogin, logarUsuario);
 
