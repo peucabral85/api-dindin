72aa48fecf2b7cd9f805da56d5b37dfa76dcaa6b
Pericles Cabral <periclesccabral@gmail.com>
2024-06-17 21:33:26 -0300

    inserindo rota de categorias

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { serverPort } = require('./configs/configs');
 const rotasUsuarios = require('./routes/usuarios');
 const rotasTransacoes = require('./routes/transacoes');
+const rotasCategorias = require('./routes/categorias');
 
 const app = express();
 
 app.use(express.json());
 app.use(rotasUsuarios);
 app.use(rotasTransacoes);
+app.use(rotasCategorias);
 
 app.listen(serverPort, () =>
     console.log(`Server is running on port ${serverPort}`));
