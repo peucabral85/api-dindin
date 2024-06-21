6a5a36bdcd170cb5f149cafbb986b4f1f17ad45f
Pericles Cabral <periclesccabral@gmail.com>
2024-06-21 12:29:54 -0300

    inserindo validação do campo valor

--- src/middlewares/validacoesTransacoes.js
+++ src/middlewares/validacoesTransacoes.js
@@ -2,7 +2,11 @@ const validarCamposTransacao = (req, res, next) => {
     const { descricao, valor, data, categoria_id, tipo } = req.body;
 
     if (!descricao || !valor || !data || !categoria_id || !tipo) {
-        return res.status(400).json({ mensagem: "É obrigatório informar todos os dados." })
+        return res.status(400).json({ mensagem: "É obrigatório informar todos os dados." });
+    }
+
+    if (valor <= 0 || isNaN(valor)) {
+        return res.status(400).json({ mensagem: "Valor informado é inválido." });
     }
 
     if (tipo.toLowerCase() !== "entrada" && tipo.toLowerCase() !== 'saida') {
